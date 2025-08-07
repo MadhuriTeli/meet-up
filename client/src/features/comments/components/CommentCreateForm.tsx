5201bbba5d76590aae7d1e014db974219accf996
MadhuriTeli <madhuriteli07@gmail.com>
2025-08-07 22:13:06 +0530

    CommentCreateForm

---
+++ client/src/features/comments/components/CommentCreateForm.tsx
@@ -0,0 +1,14 @@
+import { Experience } from "@advanced-react/server/database/schema";
+
+
+type CommentCreateFormProps = {
+    experienceId : Experience["id"];
+}
+
+export function CommentCreateForm({ experienceId }: CommentCreateFormProps) {
+    return (
+        <div>
+
+        </div>
+    )
+}
